1bb927b2e60a4b468213c0f49372695990fefcca
FlowerBoy-SkullGirl <dmurphyshort@gmail.com>
2022-03-29 20:40:47 -0400

    Added curses to bcdm, segfault

---
+++ bcdm/src/curses.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ncurses.h>
+
+int main(){
+	initscr();
+	return 0;
+}
--- bcdm/src/main.c
+++ bcdm/src/main.c
@@ -11,10 +11,15 @@
 #include <string.h>
 #include <ncurses.h>
 
+//CONSTANTS
 #define MAX_STR 256
 #define USR_FILE "/etc/passwd"
 #define USR_RANGE_MIN 999
 #define USR_RANGE_MAX 6000
+
+//CURSES
+#define HEIGHT 10
+#define WIDTH 30
 
 struct usrList{
 	struct usrList *next;
@@ -22,7 +27,7 @@ struct usrList{
 };
 
 //Argument 1 is the head of the list. Argument 2 will be the uname of the List node
-void push_usrList(struct usrList *head, char *src)
+struct usrList *push_usrList(struct usrList *head, char *src)
 {
 	if (head == NULL){
 		head = malloc(sizeof(struct usrList));
@@ -30,7 +35,7 @@ void push_usrList(struct usrList *head, char *src)
 		head->uname = NULL;
 	}
 	if (head->next != NULL){
-		push_usrList(head->next, src);
+		head->next = push_usrList(head->next, src);
 	}else{
 		struct usrList *np = malloc(sizeof(struct usrList));
 		if (np == NULL){
@@ -44,18 +49,23 @@ void push_usrList(struct usrList *head, char *src)
 			exit(0);
 		}
 		strcpy(np->uname, src);
-		
+
 		if (head->uname == NULL)
 			head = np;
 		else
 			head->next = np;
 	}
+	return head;
 }
 
 //Function will pop entire list and deallocate memory along the way
 void pop_usrList(struct usrList *head)
 {
-	puts("Popping");
+	if (head == NULL){
+		fprintf(stderr, "NULL list");
+		exit(0);
+	}
+	
 	if (head->next != NULL)
 		pop_usrList(head->next);
 	if (head != NULL){
@@ -74,34 +84,30 @@ int parse_usr(char *src)
 {
 	int i = 0;
 	int j = 0;
-	char temp[MAX_STR];
-	char temp_uid[MAX_STR];
+	int k = 0;
+	char temp[MAX_STR]; //holds uname
+	char temp_uid[MAX_STR]; //holds UID
 
-	while (*(src) != '\0' && i < 1){
-		if (*(src) == ':')
+	while (*(src+k) != '\0' && i < 3){
+		if (*(src+k) == ':')
 			i++;
-		else
-			temp[j++] = *(src);
-		src++;
+		else if (i == 0)
+			temp[j++] = *(src+k);
+		else if (i == 2)
+			temp_uid[j++] = *(src+k);
+		
+		if (i == 1 && *(src+k) == ':'){
+			temp[j] = '\0';
+			j = 0;
+		}else if (i == 3){
+			temp_uid[j] = '\0';
+		}
+		k++;
 	}
-	//No increment necessary, as this replaces the colon
-	temp[j] = '\0';
-	printf("%s\n", temp);
-	j = 0;
 
-	while (*(src) != '\0' && i < 3){
-		if (*(src) == ':')
-			i++;
-		if (i >= 2 && *src != ':')
-			temp_uid[j++] = *(src);
-		src++;
-	}
-	temp_uid[j] = '\0';
-	printf("%s\n", temp_uid);
 	//USR_RANGE determines which UIDs containt human users, then we copy that uname to src
 	if (atoi(temp_uid) > USR_RANGE_MIN && atoi(temp_uid) < USR_RANGE_MAX){
 		strcpy(src, temp);
-		printf("%s\n", src);
 		return 1;
 	}
 
@@ -125,8 +131,9 @@ struct usrList *fetch_usrs()
 	}
 
 	while (fgets(buf, MAX_STR, fp)){
-		if (parse_usr(buf))
-			push_usrList(np, buf);	
+		if (parse_usr(buf)){
+			np = push_usrList(np, buf);	
+		}
 	}
 	
 	fclose(fp);
@@ -138,26 +145,105 @@ struct usrList *fetch_usrs()
 int count_list(struct usrList *np)
 {
 	int i = 1;
+	if (np == NULL)
+		return 0;
+
 	for (; np->next != NULL; i++)
 		np = np->next;
 	return i;
 }
 
 //Replace this with curses after testing
-void display_usrList(struct usrList *head)
+void display_usrList(struct usrList *head, WINDOW *win, int highlight, int count)
 {
-	puts("Displaying");
-	while (head->next != NULL && head != NULL){
-		printf("%s\n", head->uname);
-		head = head->next;
+	int a = 2;
+	int b = 2;
+	char *menu[count];
+	
+	if (head == NULL){
+		fprintf(stderr, "NULL HEAD");
+		exit(0);
 	}
+	
+	struct usrList *np = head;
+	for (int i = 0; i < count; i++){
+		menu[i] = np->uname;
+		np = np->next;
+	}
+
+
+	box(win, 0, 0);
+	for(int c = 0; c < count; c++){
+		if (highlight == c + 1){
+			wattron(win, A_REVERSE);
+			mvwprintw(win, b, a, "%s", menu[c]);
+			wattroff(win, A_REVERSE);
+		}else{
+			mvwprintw(win, b, a, "%s", menu[c]);
+		}
+		b++;
+	}
+	wrefresh(win);
+}
+
+int menu_usrList(struct usrList *np)
+{
+	WINDOW *usrs;
+	int highlight = 1;
+	int choice = 0;
+	int usrcount = count_list(np);
+	int x = (80 - WIDTH) / 2;
+	int y = (24 - HEIGHT) / 2;
+	int ch;
+	
+	initscr();
+	noecho();
+	cbreak();
+	
+	clear();
+	keypad(usrs, TRUE);
+	usrs = newwin(HEIGHT, WIDTH, y, x);
+	while (choice == 0){
+		display_usrList(np, usrs, highlight, usrcount);
+		while(ch = wgetch(usrs)){
+			switch(ch){
+			case 65: //key_up
+				if(highlight == 1){
+					highlight = usrcount; //loop menu to bottom
+				}
+				else{
+					--highlight;
+				}
+				break;
+			case 66: //key_down
+				if(highlight == usrcount){
+					highlight = 1; //loop menu back to top
+				}
+				else{
+					++highlight;
+				}
+				break;
+			case 10: //user hits enter
+				choice = highlight;
+				break;
+			default:
+				break;
+			}
+			if(choice != 0){ //choice has been made
+				break;
+			}
+			display_usrList(np, usrs, highlight, usrcount);
+		}
+	}
+	endwin();
+	return choice;
 }
 
 int main()
 {
 	struct usrList *np = fetch_usrs();
 	
-	display_usrList(np);
+	menu_usrList(np);
 
 	//Essential to deallocate used memory!!
 	pop_usrList(np);
