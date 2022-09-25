968290a6652fe18c43cc31ac9ed56bfd2c04dcaf
FlowerBoy-SkullGirl <dmurphyshort@gmail.com>
2022-09-25 15:21:17 -0400

    Add: Linked list implementation for masses in gravitas to avoid rewriting code. Ncurses output of data points as well as representation of masses at scale. Rewrote certain methods to use Kg in place of g and rewrote #define directives to follow suit. Current development is in test.c

--- gravitas/source/test.c
+++ gravitas/source/test.c
@@ -2,16 +2,33 @@
 #include <math.h>
 #include <ncurses.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-
+//Measurements in kilograms and meters
 #define G_CONSTANT 6.674e-11
 #define PI 3.1415
-#define TICK 1
+#define TICK 100000
 #define TEST_MASS_1 10e11
 #define TEST_MASS_2 10e11
 #define TEST_MASS_3 10e9
-#define SCALE 1
+#define SCALE 2e10
 #define RAD_ADJ PI / 180.0
+#define MASS_EARTH 5.972e24
+#define MASS_SUN 1.989e30
+#define MASS_MOON 7.342e22
+#define MASS_JUPITER 1.899e27
+#define MASS_MARS 6.39e23
+#define DISTANCE_EM 3.844e8
+#define DISTANCE_ES 1.5e11
+#define DISTANCE_JS 7.41e11
+#define DISTANCE_MS 2.16e11
+#define I_VEL_E 2.98e4
+#define I_VEL_M 1.022e3
+#define I_VEL_MARS 2.4e4
+#define I_VEL_J 1.307e4
+#define T_SLEEP 66000
+#define SIGNIFICANT_RADIANS 0.1
+
 
 
 /*
@@ -32,6 +49,8 @@
  * fabs, abs, 
  */
 
+double time_passed_total = 0;
+
 struct vector_3{
 	double magnitude;
 	double x_comp;
@@ -51,7 +70,6 @@ struct massive_object{
 };
 
 struct mass_list{
-	//int id;
 	struct massive_object m_obj;
 	struct mass_list *next;
 };
@@ -82,12 +100,12 @@ void component_3d(struct vector_3 *vect)
 
 void component_3d_alt(struct vector_3 *vect)
 {
-	if (vect->gamma){
+	if (vect->gamma && vect->gamma > SIGNIFICANT_RADIANS){
 		vect->y_comp = vect->magnitude * cos(vect->gamma);
 		double hypot_xz = vect->magnitude * sin(vect->gamma);
 		vect->x_comp = hypot_xz * cos(vect->alpha);
 		vect->z_comp = hypot_xz * sin(vect->alpha);
-	}else if (vect->alpha){
+	}else if (vect->alpha && vect->alpha > SIGNIFICANT_RADIANS){
 		vect->x_comp = vect->magnitude * cos(vect->alpha);
 		vect->z_comp = vect->magnitude * sin(vect->alpha);
 	}else{
@@ -121,6 +139,8 @@ void find_angle_vector(struct vector_3 *dest, struct massive_object mo1, struct 
 double force_gravity(struct massive_object mo1, struct massive_object mo2)
 {
 	double radius = hypot_3d(mo1, mo2);
+	if (mo1.mass == MASS_EARTH)
+		mvprintw(5, 12, "Radius ES: %e", radius);
 	//printf("Object radius: %f\n", radius);
 	return G_CONSTANT * ((mo1.mass * mo2.mass)/(radius * radius));
 }
@@ -129,7 +149,9 @@ double force_gravity(struct massive_object mo1, struct massive_object mo2)
 double acceleration_due_to_gravity(struct massive_object mo1, struct massive_object mo2)
 {
 	//printf("Force of gravity between MOs: %EN\n", force_gravity(mo1, mo2));
-	return (force_gravity(mo1, mo2)/mo1.mass);
+	if (mo1.mass == MASS_EARTH && mo2.mass == MASS_SUN)
+		mvprintw(7, 30, "FG: %e", force_gravity(mo1,mo2));
+	return (force_gravity(mo1, mo2)/(mo1.mass));
 	
 }
 
@@ -142,6 +164,8 @@ struct vector_3 vector_acc_per_tick(struct massive_object mo1, struct massive_ob
 {
 	struct vector_3 v_acc;
 	v_acc.magnitude = acceleration_due_to_gravity(mo1, mo2) * TICK;
+	if (mo1.mass == MASS_EARTH)
+		mvprintw(9, 5, "E V_ACC: %e", v_acc.magnitude);
 	find_angle_vector(&v_acc, mo1, mo2);
 	component_3d_alt(&v_acc);
 	
@@ -151,7 +175,10 @@ struct vector_3 vector_acc_per_tick(struct massive_object mo1, struct massive_ob
 		v_acc.y_comp *= -1.0;
 	if (mo1.loc_z > mo2.loc_z)
 		v_acc.z_comp *= -1.0;
-	
+	if (mo1.mass == MASS_EARTH){
+		mvprintw(15,5, "E V_ACC X, Y: %e, %e", v_acc.x_comp, v_acc.y_comp);
+		mvprintw(13,5, "E V_ACC y, a, b: %e, %e, %e", v_acc.gamma, v_acc.alpha, v_acc.beta);
+	}
 	//printf("Magnitude total %e\n", v_acc.magnitude);
 	//printf("Vector x_comp: %f\n", v_acc.x_comp);
 	//printf("Vector y_comp: %f\n", v_acc.y_comp);
@@ -213,27 +240,32 @@ struct mass_list *remove_massive_object(struct mass_list *root, int index)
 	return root;
 }
 
-//Takes an index to decide which object will be calculating its new vector
-void gravity_list_iterate(struct mass_list *root, int index)
-{
-	struct mass_list *np = root + (index * sizeof(struct mass_list));
-	struct mass_list *tp = root;
-	struct vector_3 v_change;
-	for (int i = 0; tp !=NULL; i++){
-		if (tp != np){
-			v_change = vector_acc_per_tick(&(np->m_obj), &(tp->m_obj));
-			np->m_obj.vect = add_vectors(&np->m_obj.vect, &v_change);
-		}
-		tp = tp->next; 
-	}
-		
-}
-
 void update_location(struct mass_list *np)
 {
 	np->m_obj.loc_x += np->m_obj.vect.x_comp * TICK;
 	np->m_obj.loc_y += np->m_obj.vect.y_comp * TICK;
 	np->m_obj.loc_z += np->m_obj.vect.z_comp * TICK;
+}
+
+
+//Takes an index to decide which object will be calculating its new vector
+void gravity_list_iterate(struct mass_list *root, int index)
+{
+	struct mass_list *np = root;
+	for (int i = 0; i < index; i++){
+		if (np->next != NULL)
+			np = np->next;
+	}
+	struct mass_list *tp = root;
+	struct vector_3 v_change;
+	for (int i = 0; tp !=NULL; i++){
+		if (tp != np){
+			v_change = vector_acc_per_tick((np->m_obj), (tp->m_obj));
+			np->m_obj.vect = add_vectors(&np->m_obj.vect, &v_change);
+		}
+		tp = tp->next; 
+	}
+		
 }
 
 //Simulation setup
@@ -248,7 +280,7 @@ int main()
 	ncur_x /= 2;
 	ncur_y /= 2;
 
-
+/*
 	struct massive_object mo1, mo2, mo3;
 	mo1.mass = TEST_MASS_1;
 	mo2.mass = TEST_MASS_2;
@@ -260,7 +292,92 @@ int main()
 	mo2.loc_x = -10.0;
 	mo2.loc_y = 0.0;
 	mo2.loc_z = 0.0;
+*/
+	struct mass_list *root = new_node();
+	struct mass_list *cp = new_massive_object(root, MASS_SUN);
+
+	cp->m_obj.loc_x = 0.0;
+	cp->m_obj.loc_y = 0.0;
+	cp->m_obj.loc_z = 0.0;
+
+	struct vector_3 empty;
+	empty.magnitude = 0.0;
+	empty.x_comp = 0.0;
+	empty.y_comp = 0.0;
+	empty.z_comp = 0.0;
+
+	cp->m_obj.vect = empty;
+
+	struct mass_list *ep;
+	cp = new_massive_object(root, MASS_EARTH);
 	
+	cp->m_obj.loc_x = DISTANCE_ES;
+	cp->m_obj.loc_y = 0.0;
+	cp->m_obj.loc_z = 0.0;
+
+	struct vector_3 earth_init;
+	earth_init.magnitude = I_VEL_E;
+	earth_init.x_comp = 0.0;
+	earth_init.y_comp = I_VEL_E;
+	earth_init.z_comp = 0.0;
+	cp->m_obj.vect = earth_init;
+	ep = cp;
+
+	struct mass_list *mp;
+	cp = new_massive_object(root, MASS_MOON);
+	cp->m_obj.loc_x = DISTANCE_ES;
+	cp->m_obj.loc_y = DISTANCE_EM;
+	cp->m_obj.loc_z = 0.0;
+
+	struct vector_3 moon_init;
+	moon_init.magnitude = I_VEL_M;
+	moon_init.x_comp = I_VEL_M;
+	moon_init.y_comp = 0.0;
+	moon_init.z_comp = 0.0;
+	moon_init = add_vectors(&earth_init, &moon_init);
+	cp->m_obj.vect = moon_init;
+	mp = cp;
+
+	struct mass_list *jp;
+	cp = new_massive_object(root, MASS_JUPITER);
+	cp->m_obj.loc_x = DISTANCE_JS;
+	cp->m_obj.loc_y = 0.0;
+	cp->m_obj.loc_z = 0.0;
+
+	struct vector_3 jup_vel;
+	jup_vel.magnitude = I_VEL_J;
+	jup_vel.y_comp = I_VEL_J;
+	jup_vel.x_comp = 0.0;
+	jup_vel.z_comp = 0.0;
+	cp->m_obj.vect = jup_vel;
+	jp = cp;
+
+	cp = new_massive_object(root, MASS_MARS);
+	cp->m_obj.loc_x = 0.0;
+	cp->m_obj.loc_y = DISTANCE_MS * -1.0;
+	cp->m_obj.loc_z = 0.0;
+	cp->m_obj.vect.magnitude = I_VEL_MARS;
+	cp->m_obj.vect.x_comp = I_VEL_MARS;
+	cp->m_obj.vect.y_comp = 0.0;
+	cp->m_obj.vect.z_comp = 0.0;
+	struct mass_list *marsp = cp;
+	
+	/*
+	cp = new_massive_object(root, MASS_SUN / 25.0);
+
+	cp->m_obj.loc_x = DISTANCE_ES * -6.0;
+	cp->m_obj.loc_y = 0.0;
+	cp->m_obj.loc_z = 0.0;
+
+	struct vector_3 wowie;
+	wowie.magnitude = 0.0;
+	wowie.x_comp = I_VEL_MARS * 0.3;
+	wowie.y_comp = I_VEL_J * -0.3; 
+	wowie.z_comp = 0.0;
+
+	cp->m_obj.vect = wowie;
+	*/
+/*
 	struct vector_3 v_init_mo1, v_init_mo2, v_init_mo3;
 	v_init_mo1.magnitude = 2.0;
 	v_init_mo2.magnitude = 2.0;
@@ -272,12 +389,10 @@ int main()
 
 	v_init_mo3.alpha = 0.0;
 	v_init_mo3.beta = 0.0;
-/*
 	puts("Comp vi1");
 	component_3d(&v_init_mo1);
 	puts("Comp vi2");
 	component_3d(&v_init_mo2);
-*/
 	v_init_mo1.x_comp = 1.0;
 	v_init_mo1.y_comp = 1.0;
 	v_init_mo1.z_comp = 0.0;
@@ -295,99 +410,43 @@ int main()
 	mo1.vect = v_init_mo1;
 	mo2.vect = v_init_mo2;
 	mo3.vect = v_init_mo3;
-	
-	long coord_x1, coord_y1, coord_z1, coord_x2, coord_y2, coord_z2, coord_x3, coord_y3, coord_z3 = 0;
-	struct vector_3 v_change_1 = vector_acc_per_tick(mo1, mo2);
-	struct vector_3 v_change_2 = vector_acc_per_tick(mo2, mo1);
-	struct vector_3 v_change_3;
+*/	
+	long coord_x, coord_y, coord_z = 0;
 
-	char c;
-	for (int i = 0; c != 'q'; i++){
-		clear();
-		mo1.loc_x += mo1.vect.x_comp * TICK;
-		mo1.loc_y += mo1.vect.y_comp * TICK;
-		mo1.loc_z += mo1.vect.z_comp * TICK;	
+	erase();
 
-		mo2.loc_x += mo2.vect.x_comp * TICK;
-		mo2.loc_y += mo2.vect.y_comp * TICK;
-		mo2.loc_z += mo2.vect.z_comp * TICK;	
-
-		mo3.loc_x += mo3.vect.x_comp * TICK;
-		mo3.loc_y += mo3.vect.y_comp * TICK;
-		mo3.loc_z += mo3.vect.z_comp * TICK;	
-
-		if (i == 0){
-			mo1.loc_x = 10.0;
-			mo1.loc_y = 0.0;
-			mo1.loc_z = 0.0;
-	
-			mo2.loc_x = -10.0;
-			mo2.loc_y = 0.0;
-			mo2.loc_z = 0.0;
-
-			mo3.loc_x = 0.0;
-			mo3.loc_y = 0.0;
-			mo3.loc_z = 1.0;
-		}
-
-
-		coord_x1 = mo1.loc_x / SCALE;
-		coord_y1 = mo1.loc_y / SCALE;
-		coord_z1 = mo1.loc_z / SCALE;
-		coord_x2 = mo2.loc_x / SCALE;
-		coord_y2 = mo2.loc_y / SCALE;
-		coord_z2 = mo2.loc_z / SCALE;
-
-		//printf("MO1i: [%d, %d, %d]\n", coord_x1, coord_y1, coord_z1);
-		//printf("MO2i: [%d, %d, %d]\n", coord_x2, coord_y2, coord_z2);
-
-	
-		//puts("Mo1 calc");
-		v_change_1 = vector_acc_per_tick(mo1, mo2);
-		mo1.vect = add_vectors(&mo1.vect, &v_change_1);
-
-		v_change_1 = vector_acc_per_tick(mo1, mo3);
-		mo1.vect = add_vectors(&mo1.vect, &v_change_1);
-
-		//puts("Mo2 calc");
-		v_change_2 = vector_acc_per_tick(mo2, mo1);
-		mo2.vect = add_vectors(&mo2.vect, &v_change_2);
-
-		v_change_2 = vector_acc_per_tick(mo2, mo3);
-		mo2.vect = add_vectors(&mo2.vect, &v_change_2);
-
-		v_change_3 = vector_acc_per_tick(mo3, mo1);
-		mo3.vect = add_vectors(&mo3.vect, &v_change_3);
-
-		v_change_3 = vector_acc_per_tick(mo3, mo2);
-		mo3.vect = add_vectors(&mo3.vect, &v_change_3);
-
+	char SYMBOLS[] = {'S', 'E', 'm', 'J', 'M', '$'};
+	while (true){
 		
-		coord_x1 = mo1.loc_x / SCALE;
-		coord_y1 = mo1.loc_y / SCALE;
-		coord_z1 = mo1.loc_z / SCALE;
-		coord_x2 = mo2.loc_x / SCALE;
-		coord_y2 = mo2.loc_y / SCALE;
-		coord_z2 = mo2.loc_z / SCALE;
+		int i = 0;
+		time_passed_total += TICK;
+		for (cp = root; cp != NULL; cp = cp->next){
+			coord_x = cp->m_obj.loc_x / SCALE;
+			coord_y = cp->m_obj.loc_y / SCALE;
+			coord_z = cp->m_obj.loc_z / SCALE;
+			
+			gravity_list_iterate(root, i);
+			mvprintw(coord_y + ncur_y, coord_x + ncur_x, "%c", SYMBOLS[i]);
 
-		coord_x3 = mo3.loc_x / SCALE;
-		coord_y3 = mo3.loc_y / SCALE;
-		coord_z3 = mo3.loc_z / SCALE;
+			if (cp->m_obj.mass == MASS_EARTH){
+				mvprintw(4, ncur_x + 20, "Current v: [%e,%e,%e]", root->next->m_obj.vect.x_comp, root->next->m_obj.vect.y_comp, root->next->m_obj.vect.z_comp);
+				mvprintw(6, ncur_x + 20, "Mass: %e", root->next->m_obj.mass);
+				mvprintw(8, ncur_x + 20, "Pos Earth: [%e, %e, %e]", root->next->m_obj.loc_x, root->next->m_obj.loc_y, root->next->m_obj.loc_z); 
+				mvprintw(9, ncur_x + 20, "Time s: %e", time_passed_total);
 
-		//printf("MO1f: [%d, %d, %d]\n", coord_x1, coord_y1, coord_z1);
-		//printf("MO2f: [%d, %d, %d]\n", coord_x2, coord_y2, coord_z2);
-
-		mvprintw(coord_y1 + ncur_y, coord_x1 + ncur_x, "1");
-		mvprintw(coord_y2 + ncur_y, coord_x2 + ncur_x, "2");
-		mvprintw(coord_y3 + ncur_y, coord_x2 + ncur_x, "3");
+			}
+		i++;
+		}
+		for (cp = root; cp != NULL; cp = cp->next)
+			update_location(cp);
 
 		refresh();
-		usleep(50000);
+		usleep(T_SLEEP);
+		erase();
 	}
 
 	endwin();
 
-	double radius = hypot_3d(mo1, mo2);	
 	//printf("Radius: %E\n", radius);
 
 	return 0;
